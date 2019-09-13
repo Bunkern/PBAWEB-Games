1df805773ae068e1060f1af96e60729fff52feef
kenneth kruse Sørensen <Bunkern@users.noreply.github.com>
2019-09-13 23:48:22 +0200

    Start GAN Game

---
+++ PBAGAMES/Assets/NumberToGuess.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberToGuess : MonoBehaviour
+{
+
+    private System.Random random = new System.Random();
+    public int numberToGuess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        numberToGuess = random.Next(1, 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
