2da77126bc1555a93cf84cb4bb45b6c6a36737c9
kenneth kruse Sørensen <Bunkern@users.noreply.github.com>
2019-09-14 09:44:37 +0200

    Post to Database and User Script

--- PBAGAMES/Assets/NumberToGuess.cs
+++ PBAGAMES/Assets/NumberToGuess.cs
@@ -1,12 +1,20 @@
-﻿using System.Collections;
+﻿using Proyecto26;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NumberToGuess : MonoBehaviour
 {
+    public Text scoreText;
+    public InputField nameText;
 
     private System.Random random = new System.Random();
     public int numberToGuess;
+
+    //There can be only one
+    public static int playerScore;
+    public static string playerName;
 
     // Start is called before the first frame update
     void Start()
@@ -14,9 +22,18 @@ public class NumberToGuess : MonoBehaviour
         numberToGuess = random.Next(1, 10);
     }
 
-    // Update is called once per frame
-    void Update()
+    //When the submit button is activated
+    public void OnSubmit()
     {
-        
+        playerName = nameText.text;
+        PostToDatabase();
     }
+
+    //Post data to database
+    private void PostToDatabase()
+    {
+        User user = new User();
+        RestClient.Post("https://pba-web-52d02.firebaseio.com/.json", user);
+    }
+
 }
