a252169d769f0d1e986b6e753bebcdd8c4f6cc2b
kenneth kruse Sørensen <Bunkern@users.noreply.github.com>
2019-09-15 18:11:19 +0200

    TTT ready for Logic

---
+++ PBAGAMES/Assets/TTTUser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class TTTUser 
+{
+    public string userName;
+    public string userGame;
+    public int userScore;
+    public int userNoOfGames;
+
+    public TTTUser()
+    {
+        userName = TTTLogic.playerName;
+        userScore = TTTLogic.playerScore;
+        userGame = TTTLogic.playerGame;
+        userNoOfGames = TTTLogic.playerNoOfGames;
+    }
+}
